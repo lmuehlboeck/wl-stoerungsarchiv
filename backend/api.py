b63f373e1dbf08d1984d1e7219f963696dd845c4
leo <lmuehlboeck@student.tgm.ac.at>
2022-10-27 09:59:09 +0200

    Filter- & Sortierfunktionen in API fertiggestellt

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
-from email.policy import default
 from flask import Flask, g, request
 from db_management import get_conn, execute_query
+import datetime
 
 
 app = Flask(__name__)
@@ -11,13 +11,30 @@ def get_db():
         db = g._database = get_conn()
     return db
 
+def to_date(date_str):
+    if not date_str:
+        return None
+    try:
+        return datetime.datetime.strptime(date_str, "%Y-%m-%d").replace(hour=23, minute=59, second=59)
+    except ValueError:
+        raise ValueError(f"{date_str} is not a valid date in the format YYYY-MM-DD")
+
 @app.route("/disturbances", methods=["GET"])
 def api_get_disturbances():
-    res = []
+    data = []
 
-    # filters
+    # args
     lines = request.args.get("line", type=str)
     types = request.args.get("type", type=str)
+    try:
+        start_date = to_date(request.args.get("start"))
+        end_date = to_date(request.args.get("end"))
+    except ValueError as exc:
+        return {"error": {"code": 400, "message": str(exc)}}
+    order = request.args.get("order", "start", type=str)
+    desc = request.args.get("desc", type=str)
+
+    # filters
     sql = "SELECT * FROM disturbances WHERE 1=1"
     args = []
     if lines:
@@ -28,6 +45,26 @@ def api_get_disturbances():
         types = types.split(",")
         sql += f" AND type IN ({','.join(['?'] * len(types))})"
         args += types
+    if start_date:
+        sql += " AND start_time >= ?"
+        args.append(start_date)
+    if end_date:
+        sql += " AND start_time <= ?"
+        args.append(end_date)
+
+    # ordering
+    if not order or order == "start":
+        sql += " ORDER BY start_time"
+    elif order == "end":
+        sql += " ORDER BY end_time"
+    elif order == "type":
+        sql += " ORDER BY type"
+    else:
+        return {"error": {"code": 400, "message": f"Ordering by {order} is not valid"}}
+    if not desc or desc == "true":
+        sql += " DESC"
+    elif desc != "false":
+        return {"error": {"code": 400, "message": f"{desc} is not a valid boolean value"}}
 
     # generate disturbance dictionary
     disturbances = execute_query(get_db(), sql, tuple(args))
@@ -44,23 +81,26 @@ def api_get_disturbances():
                 lines_dict[l[0]] = execute_query(get_db(), "SELECT type FROM lines WHERE id=?", (l[0],))[0][0]
             
             
-            d = {
+            data.append({
                 "title": disturbance[1],
                 "descriptions": desc_list,
                 "lines": lines_dict,
                 "start_time": disturbance[3],
                 "end_time": disturbance[4],
-            }
-            res.append(d)
-    return res
+            })
+    return {
+        "data": data
+    }
 
 @app.route("/lines", methods=["GET"])
 def api_get_lines():
-    res = {}
+    data = {}
     lines_db = execute_query(get_db(), "SELECT * FROM lines ORDER BY type")
     for l in lines_db:
-        res[l[0]] = l[1]
-    return res
+        data[l[0]] = l[1]
+    return {
+        "data": data
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
