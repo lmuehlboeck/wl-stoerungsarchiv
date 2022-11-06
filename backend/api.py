2760b160032f64780db03d5031322f4a5d881d29
leo <lmuehlboeck@student.tgm.ac.at>
2022-11-06 14:52:58 +0100

    Sortierungsfehler bei /api/lines behoben

--- backend/api.py
+++ backend/api.py
@@ -113,8 +113,8 @@ def api_get_disturbances():
 @app.route("/api/lines", methods=["GET"])
 def api_get_lines():
     data = {}
-    # correct sorting - 1. type; 2. line number smallest to biggest - exception in metro rows because of incorrect casting
-    lines_db = execute_query(get_db(), "SELECT * FROM lines ORDER BY type, CASE WHEN id LIKE 'U%' THEN id ELSE cast(id as INTEGER) END")
+    # correct sorting - 1. type; 2. line number smallest to biggest (casting necessary) - except line number starts with U or N to ensure right casting
+    lines_db = execute_query(get_db(), "SELECT * FROM lines ORDER BY type, CASE WHEN id LIKE 'U%' OR id LIKE 'N%' THEN CAST(SUBSTR(id, 2) AS INTEGER) ELSE CAST(id AS INTEGER) END")
     data = [{"id":l[0], "type":l[1]} for l in lines_db]
     return {
         "data": data
